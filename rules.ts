7f97da827cdb0d9775ce41c2bea3585fafd410a1
whj <emailwen@qq.com>
2026-03-03 15:43:39 +0800

    update

--- rules.ts
+++ rules.ts
@@ -4,65 +4,66 @@
  * 在此处添加、修改、删除规则集配置
  */
 
-import type { RuleSetConfig } from './types'
+import type { RuleSetConfig } from "./types";
 
 export const rulesets: RuleSetConfig[] = [
   {
-    name: 'ai',
-    behavior: 'domain',
+    name: "ai",
+    behavior: "domain",
     rules: {
       domain_suffix: [
-        'chatgpt.com',
-        'claude.ai',
-        'zed.dev',
-        'anthropic.com',
-        'claude.com',
-        'cursor.sh'
-      ]
-    }
+        "chatgpt.com",
+        "claude.ai",
+        "zed.dev",
+        "anthropic.com",
+        "claude.com",
+        "cursor.sh",
+      ],
+    },
   },
   {
-    name: 'default',
-    behavior: 'domain',
+    name: "default",
+    behavior: "domain",
     rules: {
       domain_suffix: [
-        'turborepo.com',
-        'turbo.build',
-        'rolldown.rs',
-        'prisma.io',
-        'prisma-data.net',
-        'elysiajs.com',
-        'rollupjs.org'
-      ]
-    }
+        "turborepo.com",
+        "turbo.build",
+        "rolldown.rs",
+        "prisma.io",
+        "prisma-data.net",
+        "elysiajs.com",
+        "rollupjs.org",
+      ],
+    },
   },
   {
-    name: 'direct',
-    behavior: 'classical',
+    name: "direct",
+    behavior: "classical",
     rules: {
-      domain_suffix: ['wd-gold.com', 'wenhongjie.tech'],
-      domain_keyword: ['tagxx', 'tagss'],
-      ip_cidr: ['104.168.21.132/32', '43.154.3.205/32'],
-      domain: ['api.ikuncode.cc', "aihubmix.com"]
-    }
+      domain_suffix: ["wd-gold.com", "wenhongjie.tech"],
+      domain_keyword: ["tagxx", "tagss"],
+      ip_cidr: ["104.168.21.132/32", "43.154.3.205/32"],
+      domain: ["api.ikuncode.cc", "aihubmix.com"],
+    },
   },
   {
-    name: 'gemini',
-    behavior: 'domain',
+    name: "gemini",
+    behavior: "domain",
     rules: {
       domain: [
-        'gemini.google.com',
-        'one.google.com',
-        'aistudio.google.com',
-        'ai.google.dev',
-        'apis.google.com',
-        'www.google-analytics.com'
+        "gemini.google.com",
+        "one.google.com",
+        "aistudio.google.com",
+        "ai.google.dev",
+        "apis.google.com",
+        "www.google-analytics.com",
+        "accounts.google.com",
       ],
       domain_suffix: [
-        'googleapis.com',
-        'makersuite.google.com',
-        'generativeai.google',
-        'clients6.google.com',
+        "googleapis.com",
+        "makersuite.google.com",
+        "generativeai.google",
+        "clients6.google.com",
         "gemini.google",
         "deepmind.google",
         "opal.google",
@@ -74,7 +75,7 @@ export const rulesets: RuleSetConfig[] = [
         "opal.google.com",
         "jules.google.com",
       ],
-      domain_keyword: ['antigravity']
-    }
-  }
-]
+      domain_keyword: ["antigravity"],
+    },
+  },
+];
